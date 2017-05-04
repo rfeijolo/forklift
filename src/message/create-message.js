0977eda3639debc340d1f61b11e081a1cbbd4d59
Rodrigo Feijolo <rfeijolo@thoughtworks.com>
2017-05-03 21:51:30 -0300

    Add authorization and validation to message

--- src/message/create-message.js
+++ src/message/create-message.js
@@ -1,13 +1,32 @@
 const database = require('../database');
 const responseFactory = require('../response-factory');
+const validator = require('./validator');
 
 module.exports = createMessage;
 
 function createMessage(message, done) {
-  //validateMessage
-  //hasSameOwner
-  database.createMessage(message, handleMessageCreation);
+  database.getTopic(message.topicId, handleTopicQuery);
 
+  function handleTopicQuery(error, topic) {
+    if (error) {
+      done(null, responseFactory.genericError(error));
+      return;
+    }
+
+    const isAuthorized = validator.isAuthorized(topic, message);
+    if(!isAuthorized) {
+      done(null, responseFactory.forbidden());
+      return;
+    }
+
+    const validationResult = validator.isValid(message);
+    if(!validationResult.isValid) {
+      done(null, responseFactory.badRequest(validationResult.errors));
+      return;
+    }
+
+    database.createMessage(message, handleMessageCreation);
+  }
   function handleMessageCreation(error, createdMessage) {
     if(error) done(null, responseFactory.genericError(error));
     else done(null, responseFactory.success(createdMessage));
--- src/test/unit/message/create-message.test.js
+++ src/test/unit/message/create-message.test.js
@@ -2,20 +2,32 @@ const createMessage = require('../../../message/create-message');
 const MessageFixture = require('../../message-builder');
 const test = require('tape');
 const database = require('../../../database');
+const validator = require('../../../message/validator');
 const sinon = require('sinon');
 const responseFactory = require('../../../response-factory');
+const fixtures = require('../../fixtures');
 
 test('createMessage should return Ok', (assert) => {
   const anyMessage = new MessageFixture().build();
+  const anyTopic = fixtures.createAnyTopic();
   const anyResponse = { statusCode: 200 };
+  const getTopicStub = sinon.stub(database, 'getTopic');
+  const isAuthorizedStub = sinon.stub(validator, 'isAuthorized');
+  const isValidStub = sinon.stub(validator, 'isValid');
   const createMessageStub = sinon.stub(database, 'createMessage');
   const createTopicStub = sinon.stub(responseFactory, 'success');
+  getTopicStub.yields(null, anyTopic);
+  isAuthorizedStub.returns(true);
+  isValidStub.returns({ isValid: true });
   createTopicStub.returns(anyResponse);
   createMessageStub.yields(null, anyMessage);
 
   createMessage(anyMessage, isResponseOk);
 
   function isResponseOk(error, response){
+    getTopicStub.restore();
+    isAuthorizedStub.restore();
+    isValidStub.restore();
     createTopicStub.restore();
     createMessageStub.restore();
 
@@ -27,16 +39,26 @@ test('createMessage should return Ok', (assert) => {
 
 test('createMessage should return Internal Server Error', (assert => {
   const anyMessage = new MessageFixture().build();
+  const anyTopic = fixtures.createAnyTopic();
   const anyResponse = { statusCode: 500 };
   const expectedErrorMessage = 'An unexpected error has ocurred.';
+  const getTopicStub = sinon.stub(database, 'getTopic');
+  const isAuthorizedStub = sinon.stub(validator, 'isAuthorized');
+  const isValidStub = sinon.stub(validator, 'isValid');
   const genericErrorStub = sinon.stub(responseFactory, 'genericError');
   const createMessageStub = sinon.stub(database, 'createMessage');
+  getTopicStub.yields(null, anyTopic);
+  isAuthorizedStub.returns(true);
+  isValidStub.returns({ isValid: true });
   createMessageStub.yields(new Error(expectedErrorMessage));
   genericErrorStub.returns(anyResponse);
 
   createMessage(anyMessage, isResponseInternalServerError);
 
   function isResponseInternalServerError(error, response) {
+    getTopicStub.restore();
+    isAuthorizedStub.restore();
+    isValidStub.restore();
     createMessageStub.restore();
     genericErrorStub.restore();
 
